1bc8818971130fa6ac6681c2e850b836b023e125
scrif <andrews.austin.94@gmail.com>
2022-06-28 16:18:00 -0500

    Add video scrub function.
    Added the initial scrub function. Clicking anywhere in
    the progress bar allows the user to navigate to that point in the
    video playback.
    Still needed: update function(s) to allow user to drag back and forth
    on the progress bar to actually scrub through the video.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -44,7 +44,11 @@ function handleProgress() {
     progressBar.style.flexBasis = `${percent}%`;
 }
 
-
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(e);
+}
 
 // Connect event listeners
 
@@ -60,3 +64,4 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
+progress.addEventListener('click', scrub);
