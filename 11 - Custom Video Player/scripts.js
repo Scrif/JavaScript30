3c242e9b36d0dce2893b0f0f98fd69b16b869e3f
scrif <andrews.austin.94@gmail.com>
2022-06-28 16:28:09 -0500

    Finish scrubbing function. Add fullscreen button to player

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -50,6 +50,12 @@ function scrub(e) {
     console.log(e);
 }
 
+function openFullscreen() {
+    if (video.requestFullscreen) {
+        video.requestFullscreen();
+    }
+}
+
 // Connect event listeners
 
 video.addEventListener('click', togglePlay);
@@ -64,4 +70,9 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
+
+let mousedown = false;
 progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
