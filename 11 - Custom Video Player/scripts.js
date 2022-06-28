3780f66e372477aabde1bd423b92f6289539cc0b
scrif <andrews.austin.94@gmail.com>
2022-06-28 15:43:05 -0500

    Add querySelector variables. Add togglePlay function when video clicked.

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,29 @@
+// Get elements
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Build functions
+
+function togglePlay() {
+    // Ternary operator method:
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+
+//    if(video.paused) {
+//        video.play();
+//    } else {
+//        video.pause();
+//    }
+}
+
+
+
+// Connect event listeners
+
+video.addEventListener('click', togglePlay);
